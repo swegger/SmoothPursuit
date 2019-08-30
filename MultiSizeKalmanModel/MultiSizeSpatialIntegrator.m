91c918117ef98faf32c8889523f82c1cd22e958f
Seth Egger <sethegger@gmail.com>
2019-08-30 13:59:41 -0400

    Generalized number of temporal integrators in MultiSizeSpatialIntegrator

--- MultiSizeKalmanModel/MultiSizeSpatialIntegrator.m
+++ MultiSizeKalmanModel/MultiSizeSpatialIntegrator.m
@@ -1,4 +1,4 @@
-function zstar = MultiSizeSpatialIntegrator(varargin)
+function zstar = MultiSizeSpatialIntegrator(N,varargin)
 %%
 %
 %
@@ -10,16 +10,18 @@ function zstar = MultiSizeSpatialIntegrator(varargin)
 %% Parse inputs
 Parser = inputParser;
 
+addRequired(Parser,'N')
 addParameter(Parser,'trials',100)
 addParameter(Parser,'z',10*ones(1,200))
-addParameter(Parser,'H',ones(100,1))
-addParameter(Parser,'Wx',ones(100,1))
-addParameter(Parser,'zhat0',zeros(100,1))
-addParameter(Parser,'wT0',1e6*ones(100,1))
-addParameter(Parser,'Wt',0.5*ones(100,1))
+addParameter(Parser,'H',NaN)
+addParameter(Parser,'Wx',NaN)
+addParameter(Parser,'zhat0',NaN)
+addParameter(Parser,'wT0',NaN)
+addParameter(Parser,'Wt',NaN)
 
-parse(Parser,varargin{:})
+parse(Parser,N,varargin{:})
 
+N = Parser.Results.N;
 trials = Parser.Results.trials;
 z = Parser.Results.z;
 H = Parser.Results.H;
@@ -27,6 +29,26 @@ Wx = Parser.Results.Wx;
 zhat0 = Parser.Results.zhat0;
 wT0 = Parser.Results.wT0;
 Wt = Parser.Results.Wt;
+
+if any(isnan(H))
+    H = ones(N,1);
+end
+
+if any(isnan(Wx))
+    Wx = ones(N,1);
+end
+
+if any(isnan(zhat0))
+    zhat0 = zeros(N,1);
+end
+
+if any(isnan(wT0))
+    wT0 = 1e6*ones(N,1);
+end
+
+if any(isnan(Wt))
+    Wt = 0.5*ones(N,1);
+end
 
 %% Perform simulations
 
